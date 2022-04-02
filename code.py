8533ee184268ecc35ef8d8d51f9f2ac1443b7373
carlosviveros <66926375+carlosviveros@users.noreply.github.com>
2022-04-01 22:43:23 -0500

    primera súbida del codigo principal

---
+++ code.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from datetime import date
+
+
+def menu():
+     fecha = input("Ingresa una fecha en el formato YYYY-MM-DD: ")
+     turno1 = int(input("ingrese turno"))
+     horaIncial = int(input("ingrese hora inicial"))
+     horaFinal = int(input("ingrese fecha final"))
+
+
+def validarDate(date):
+    while True:
+        try:
+            date = input("Ingresa una fecha en el formato YYYY-MM-DD: ")
+            datetime.strptime(date, '%Y-%m-%d')
+            print("Fecha válida")
+            return date
+        except ValueError:
+            print("Fecha inválida")
+   
+
+
+def validarhora(h):
+       while True:
+        try:
+            date = input("Ingresa hora en el formato HH:MM : ")
+            datetime.strptime(h, "%H:%M")
+            print("Hora válida")
+            return h
+        except ValueError:
+              print("Hora inválida")
+    
+       
+    
+#def main():
+    
+    
+    
+       
+        
