1fda268928c6601312cc9258621ddc7806455fda
carlosviveros <66926375+carlosviveros@users.noreply.github.com>
2022-04-26 22:55:08 -0500

    Add files via upload

--- interfaz.py
+++ interfaz.py
@@ -13,17 +13,18 @@ from tkinter import (
     ttk
 )
 
-#Como su nombre lo indica, retorna la fecha de hoy
+#Como su nombre lo indica, el método _today_date retorna la fecha de hoy
 def _today_date():
     return datetime.date.today().strftime("%d/%m/%Y")
 
 
-#Con el decorador "Dataclass" se pretende facilitar la creación de clases que poseean datos. 
+#Con el decorador "Dataclass" se pretende facilitar la creación de clases. 
 @dataclass
 class Formulario:
     #Se definen los atributos de la clase con sus correspondientes tipos de datos:
     fecha: str
     turno: int
+    #Field describe el cada campo definido
     hora_inicial: str = field(default="00:00")
     hora_final: str = field(default="00:00")
     jefe_turno: str = ""
@@ -31,10 +32,13 @@ class Formulario:
     preforma: str = ""
     envase: str = ""
     produccion_total: str = ""
+    #Total_desperidicio y producción conforme tomarán un valor booleano que en este caso será falso
     total_desperdicio: str = field(init=False)
     produccion_conforme: str = field(init=False)
 #Con el método post_init se inicalizan variables que dependerán de uno o más campos 
     def __post_init__(self):
+        #en caso de no haber un valor en "desperdicio" y  "conforme", el total desperidicio y producción conforme tendrán por defecto un 0 que tra
+        
         try:
             desperdicio = float(self.preforma) + float(self.envase)
             conforme = float(self.produccion_total) - desperdicio
@@ -63,14 +67,18 @@ class Formulario:
 class Widget(Frame):
      #el método init de la clase widget tendrá dos atributos: master y turno
     def __init__(self, master, turno):
-        #Para acceder a los elementos(métodos y atributos) de la clase padre frame:
+        #Para acceder a los elementos(métodos y atributos) de la clase padre frame se hará uso del método super(Herencia):
         super().__init__(master)
         self.turno = turno
         self.setup_ui()
         #Se recorre cada widget para agregar un "espaciado" de en 5  "x" y 5 en "y"  
         for widget in self.winfo_children():
             widget.grid_configure(padx=5, pady=5)
-        #En este método se crean todos los widgets que se utilizarán, es decir, tanto labels como sus entradas de texto. Y de igual forma,  posicionarán de acuerdo al grid de las ventanas (columnas)
+        #En este método se crean todos los widgets que se utilizarán,es decir, tanto labels como entradas de texto. Y de igual forma, se establecerá la  posición de acuerdo al grid 
+        # de la ventanas (celdas)
+        #Apreciaciones:
+        #columnspan:2  Indica la posición del widget en  dos columnas
+        #sticky: expandir el widget. "e" indica la posición este
     def setup_ui(self):
         Label(self, text="Hora inicial").grid(row=1, column=0, sticky="e")
         self.ent_hora_inicial = Entry(self)
@@ -132,7 +140,7 @@ class Widget(Frame):
         )
         self.btn_informe.grid(row=14, columnspan=2)
   
-  #El método calcular captura en la instancia "formulario",  las diferentes entradas de texto
+  #El método "calcular" captura en la instancia "formulario"  las diferentes entradas de texto
     def calcular(self):
         formulario = Formulario(
             fecha=_today_date(),
@@ -146,21 +154,26 @@ class Widget(Frame):
             produccion_total=self.ent_produccion_total.get(),
         )
         self.f = formulario
-        #El texto del label "total_desperdicio" será igual al atributo de instancia .total_desperdicio. Y  de la misma forma ocurre con 
+        #El texto del label "total_desperdicio" será igual al atributo de instancia ".total_desperdicio", y  de la misma forma ocurre con 
         #produccion conforme
         self.lbl_total_desperdicio.config(text=formulario.total_desperdicio)
         self.lbl_produccion_conforme.config(text=formulario.produccion_conforme)
-        #retorna 
+        #retorna  la instancia
         return formulario
 
     def informe(self):
+        #Toplevel: ventana superior
         display = Toplevel()
         display.title("Informe")
+        #dimensiones del texto en la ventana
         w = Text(display, width=36, height=12)
+        #inserta los strings accediando al método to_str()
         w.insert("end", self.f.to_str())
+        #Para que no sea modificado el texto de la ventana, el state deberá ser  disabled
         w.config(state="disabled")
         w.pack()
-
+        
+        #"Limpia" o "resetea" las diferentes entradas 
     def limpiar(self):
         self.ent_hora_inicial.delete("0", "end")
         self.ent_hora_final.delete("0", "end")
@@ -170,11 +183,12 @@ class Widget(Frame):
         self.ent_envase.delete("0", "end")
         self.ent_produccion_total.delete("0", "end")
 
-
+#La clase Container hereda la clase frame
 class Container(Frame):
-
+  
     def __init__(self, master, turno):
         super().__init__(master)
+        #Número de columnas:3
         self.N = 3
         self.widgets = []
         self.turno = turno
@@ -184,7 +198,7 @@ class Container(Frame):
         Label(self, text=f"Fecha: {_today_date()}").grid(row=0, column=1)
         self.setup_ui()
         self.master.after(1000, self.update)
-
+#Frame de totales
     def setup_ui(self):
         for n in range(self.N):
             w = Widget(self, n + 1)
@@ -213,19 +227,24 @@ class Container(Frame):
         self.lbl_produccion.grid(row=0, column=6)
 
     def update(self):
+        #Suma los totales de desperdicio y producción de cada maquina
         desperdicio = sum([
             float(w.calcular().total_desperdicio) for w in self.widgets
         ])
         produccion = sum([
             float(w.calcular().produccion_conforme) for w in self.widgets
         ])
+        #El texto del label total_desperdicio será igual a desperdicio
         self.lbl_total_desperdicio.config(text=str(desperdicio))
+        #El texto del label porcentaje desperdicio será igual a desperdicio/100
         self.lbl_porcentaje_desperdicio.config(
             text=f"{(desperdicio / 100):.2f}%"
         )
+        #El texto del label produccion será igual a produccion
         self.lbl_produccion.config(text=str(produccion))
 
-
+#lA clase FrameScrollbar hereda la clase frame
+#En resumidas cuentas la barra de desplazamiento
 class FrameScrollbar(Frame):
 
     def __init__(self, master):
@@ -243,7 +262,7 @@ class FrameScrollbar(Frame):
     def on_configure(self, event):
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
-
+#La venta raíz:
 class App(Tk):
 
     def __init__(self, N):
@@ -269,5 +288,6 @@ class App(Tk):
 
 
 if __name__ == "__main__":
+    #Como son dos turnos, N deberá ser igual a 2, pues se precisa un número de fila por cada turno: Turno1 arriba, Turno2 abajo 
     app = App(N=2)
     app.mainloop()
